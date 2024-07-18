e720127939e4df7bc9ed9761acfdfaba313745f5
lrnbrad <lrnbrad@gmail.com>
2024-07-18 21:15:28 +0800

    Chore: Rewrite DramaController and relative models and serializers, remove unused files

--- Assets/CSharp/Model/Dialogue.cs
+++ Assets/CSharp/Model/Dialogue.cs
@@ -1,12 +1,12 @@
-using System;
+#nullable enable
+using System.Xml.Serialization;
 
 namespace CSharp.Model {
-    [Serializable]
-    public class Dialogue
-    {
-        public int no;
-        public string actor;
-        public string picture;
-        public string message;
+    public class Dialogue {
+        [XmlAttribute("name")] public string Name { get; set; }
+
+        [XmlAttribute("pic")] public string? Picture { get; set; }
+
+        [XmlText] public string Text { get; set; }
     }
 }
